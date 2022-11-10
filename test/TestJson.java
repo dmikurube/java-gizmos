671e4526ad3d31ddc9b0ae1ab2a8632e373afd0a
Dai MIKURUBE <dmikurube@acm.org>
2022-11-10 19:20:11 +0900

    Add more test cases for Json escape

--- test/TestJson.java
+++ test/TestJson.java
@@ -17,8 +17,45 @@ public class TestJson {
 
     private static Stream<Arguments> stringsToEscape() {
         return Stream.of(
+                arguments("foo\\bbar", "foo\bbar"),
+                arguments("foo\\fbar", "foo\fbar"),
                 arguments("foo\\nbar", "foo\nbar"),
+                arguments("foo\\rbar", "foo\rbar"),
+                arguments("foo\\tbar", "foo\tbar"),
+                arguments("foo\\u0000bar", "foo\u0000bar"),
+                arguments("foo\\u0001bar", "foo\u0001bar"),
+                arguments("foo\\u0002bar", "foo\u0002bar"),
+                arguments("foo\\u0003bar", "foo\u0003bar"),
+                arguments("foo\\u0004bar", "foo\u0004bar"),
+                arguments("foo\\u0005bar", "foo\u0005bar"),
+                arguments("foo\\u0006bar", "foo\u0006bar"),
+                arguments("foo\\u0007bar", "foo\u0007bar"),
+                arguments("foo\\bbar", "foo\u0008bar"),
+                arguments("foo\\tbar", "foo\u0009bar"),
+                // 000a cannot be literalized.
+                // @see https://docs.oracle.com/javase/specs/jls/se14/html/jls-3.html#jls-3.10.5
+                arguments("foo\\u000bbar", "foo\u000bbar"),
+                arguments("foo\\fbar", "foo\u000cbar"),
+                // 000d cannot be literalized.
+                // @see https://docs.oracle.com/javase/specs/jls/se14/html/jls-3.html#jls-3.10.5
+                arguments("foo\\u000ebar", "foo\u000ebar"),
+                arguments("foo\\u000fbar", "foo\u000fbar"),
+                arguments("foo\\u0010bar", "foo\u0010bar"),
+                arguments("foo\\u0011bar", "foo\u0011bar"),
+                arguments("foo\\u0012bar", "foo\u0012bar"),
+                arguments("foo\\u0013bar", "foo\u0013bar"),
+                arguments("foo\\u0014bar", "foo\u0014bar"),
+                arguments("foo\\u0015bar", "foo\u0015bar"),
+                arguments("foo\\u0016bar", "foo\u0016bar"),
+                arguments("foo\\u0017bar", "foo\u0017bar"),
+                arguments("foo\\u0018bar", "foo\u0018bar"),
+                arguments("foo\\u0019bar", "foo\u0019bar"),
+                arguments("foo\\u001abar", "foo\u001abar"),
+                arguments("foo\\u001bbar", "foo\u001bbar"),
+                arguments("foo\\u001cbar", "foo\u001cbar"),
+                arguments("foo\\u001dbar", "foo\u001dbar"),
+                arguments("foo\\u001ebar", "foo\u001ebar"),
+                arguments("foo\\u001fbar", "foo\u001fbar"));
                 arguments("いろは", "いろは"),
-                arguments("\\u0001", "\u0001"));
     }
 }
